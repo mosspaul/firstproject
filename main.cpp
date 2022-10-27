d06d4afaa9df5decf031a09e5d607b19cf18d231
mosspaul <gmosspaul18@gmail.com>
2022-10-26 19:23:08 -0500

    Add index buffer(Episode 9)

--- main.cpp
+++ main.cpp
@@ -114,21 +114,31 @@ int main(void)
     *
     *
     */
-    float positions[6] = {
-        -0.5f, -0.5f, 
-        0.0f, 0.5f, 
-        0.5f, -0.5f 
+    float positions[] = {
+        -0.5f, -0.5f, //0
+        0.5f, -0.5f, //1
+        0.5f, 0.5f, //2
+        -0.5f, 0.5f, //3
+        0.0f, 1.0f  //4
+    };
+    unsigned int indices[] {
+        0, 1, 2, 
+        0, 3, 2,
+        2, 4, 3
     };
 
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 9 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
-    
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 9 * sizeof(unsigned int), indices, GL_STATIC_DRAW);    
     /*
     * WRITING 
     * SHADER
@@ -142,7 +152,7 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, nullptr);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
