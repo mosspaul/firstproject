7474ea486d3336f5b9a58fd2c0b443e7be5e46d8
mosspaul <gmosspaul18@gmail.com>
2022-10-31 09:03:19 -0500

    Add python file for clarification

--- test/test.py
+++ test/test.py
@@ -1,19 +1,21 @@
 # Some python code to check a c++ implementation of a loop
 
 def main():
-    target = 0.00
-    increment = 0.05
+    target = 0
+    increment = 5
+    doIncrement = True
 
-    while target <= 1:
-        print(target)
-        target = round(target, 2) + increment
-        if target == 1.0:
-            break
-    if target == 1.0:
-        while target >= 0.0:
+    while True:
+        if doIncrement == True:
+            target += increment
             print(target)
-            target = round(target, 2) - increment
-            if target == 0.0:
-                break
+            if target == 100:
+                doIncrement = False
+        if doIncrement == False:
+            target -= increment
+            print(target)
+            if target == 0:
+                doIncrement = True
+        
 main()
     
